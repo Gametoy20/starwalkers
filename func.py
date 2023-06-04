3d1e947bf7f6c0458185348e6c6e303cc7c07a63
Gametoy20 <92245607+Gametoy20@users.noreply.github.com>
2023-06-04 22:17:05 +0700

    Files added

---
+++ func.py
@@ -0,0 +1,102 @@
+import random
+
+def got_let_int(ggg):
+    letters = {
+        "A": 26,
+        "B": 25,
+        "C": 24,
+        "D": 23,
+        "E": 22,
+        "F": 21,
+        "G": 20,
+        "H": 19,
+        "I": 18,
+        "J": 17,
+        "K": 16,
+        "L": 15,
+        "M": 14,
+        "N": 13,
+        "O": 12,
+        "P": 11,
+        "Q": 10,
+        "R": 9,
+        "S": 8,
+        "T": 7,
+        "U": 6,
+        "V": 5,
+        "W": 4,
+        "X": 3,
+        "Y": 2,
+        "Z": 1
+    }
+    return letters.get(ggg, 0)
+def roll():
+    #li = input()
+    #fi += 1
+    ran = random.randint(1,351)
+
+
+    if ran == 1:
+        ship_let = "A"
+    elif ran <=3:
+        ship_let = "B"
+    elif ran <=6:
+        ship_let = "C"
+    elif ran <=10:
+        ship_let = "D"
+    elif ran <=15:
+        ship_let = "E"
+    elif ran <=21:
+        ship_let = "F"
+    elif ran <=28:
+        ship_let = "G"
+    elif ran <=36:
+        ship_let = "H"
+    elif ran <=45:
+        ship_let = "I"
+    elif ran <=55:
+        ship_let = "J"
+    elif ran <=66:
+        ship_let = "K"
+    elif ran <=78:
+        ship_let = "L"
+    elif ran <=91:
+        ship_let = "M"
+    elif ran <=105:
+        ship_let = "N"
+    elif ran <=120:
+        ship_let = "O"
+    elif ran <=136:
+        ship_let = "P"
+    elif ran <=153:
+        ship_let = "Q"
+    elif ran <=171:
+        ship_let = "R"
+    elif ran <=190:
+        ship_let = "S"
+    elif ran <=210:
+        ship_let = "T"
+    elif ran <=231:
+        ship_let = "U"
+    elif ran <=253:
+        ship_let = "V"
+    elif ran <=276:
+        ship_let = "W"
+    elif ran <=300:
+        ship_let = "X"
+    elif ran <=325:
+        ship_let = "Y"
+    elif ran <=351:
+        ship_let = "Z"
+    ran = random.randint(0, 9999)
+    if ran/10 >= 1:
+        if ran/100 >= 1:
+            if ran/1000 >= 1:
+                niki = ship_let+"-"+str(ran)
+            elif ran/1000 < 1:
+                niki = ship_let+"-0"+str(ran)
+        elif ran/100 < 1:
+            niki = ship_let+"-00"+str(ran)
+    elif ran/10 < 1:
+        niki = ship_let+"-000"+str(ran)
+    return niki
---
+++ main.py
@@ -0,0 +1,98 @@
+import random
+import os
+import re
+import termcolor
+from func import roll, got_let_int
+from termcolor import colored, cprint
+print(colored("WELCOME TO STARWALKERS!", "red", attrs=["bold"]))
+
+
+let_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+summ_w = len(let_list)
+summ_all = int()
+a = int()
+for i in range(summ_w):
+    b = let_list[i]
+    a = i+1
+    summ_all += a
+    #print(b, summ_all)
+ran = int()
+fi = -1
+#print(summ_all)
+#priti = str()
+#infi = 0
+#while priti != "A-9999":
+#    priti = roll()
+#    infi += 1
+#print(infi)
+#zina = input()
+clear = lambda: os.system('cls')
+money = 100
+user_case = 0
+ship_list = []
+name = str(input("Enter your name: "))
+name_txt = name+".txt"
+try:
+    filename = open(name_txt, "r")
+    file_cont = filename.readlines()
+    lines = len(filename.readlines())
+    money = int(re.sub(r"[\n]", "", file_cont[0]))
+    user_case = int(re.sub(r"[\n]", "", file_cont[1]))
+    for j in range(len(file_cont)-2):
+        ship_list.append(re.sub(r"[\n]", "", file_cont[j+2]))
+    filename.close()
+    #print("Lines:", lines)
+except FileNotFoundError:
+    filename = open(name_txt, "w")
+    filename.write("100\n")
+    filename.write("0")
+    filename.close()
+    print("Registered:", name)
+def save():
+    filename = open(name_txt, "w")
+    filename.write(str(money)+"\n")
+    filename.write(str(user_case))
+    for k in range(len(ship_list)):
+        filename.write("\n"+ship_list[k-1])
+    filename.close()
+while True:
+    print("You have " + str(money)+"$ and "+str(user_case)+" cases.")
+    user_input=input("Menu ")
+    if user_input == "case":
+        clear()
+        user_input2 = input("Want to buy/open/close?")
+        if user_input2 == "buy":
+            clear()
+            user_input3 = int(input("How much?"))
+            if money >= 10*user_input3:
+                money -= 10*user_input3
+                user_case += user_input3
+                clear()
+                print("Thanks for buying", user_input3, "cases.")
+                save()
+            else:
+                print("Not enough money, bro.")
+        if user_input2 == "open":
+            if user_case >= 1:
+                user_case -= 1
+                gotter = roll()
+                ship_list.append(gotter)
+                #print(ship_list)
+                clear()
+                gotter_letter, gotter_int = gotter.split("-")
+                cost = (got_let_int(gotter_letter)*int(gotter_int))//1000
+                print("You got:", gotter+"! It costs:", cost)
+                save()
+    if user_input == "coll":
+        clear()
+        coli = ""
+        for ii in range(len(ship_list)):
+            if ii == len(ship_list)-1:
+                coli += ship_list[ii-1] + "."
+            else:
+                coli += ship_list[ii-1] + ", "
+        print("Your collection of ships:", coli)
+    if user_input == "exit":
+        break
+
+                
